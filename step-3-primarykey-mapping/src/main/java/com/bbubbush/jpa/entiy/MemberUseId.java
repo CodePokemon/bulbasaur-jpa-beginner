349c0b13b07112329a0f571e5e2ebb0db1de7041
bbubbush <bbubbush@gmail.com>
2020-06-26 23:45:05 +0900

    1. add primary key mapping strategy.

--- step-3-primarykey-mapping/src/main/java/com/bbubbush/jpa/entiy/MemberUseId.java
+++ step-3-primarykey-mapping/src/main/java/com/bbubbush/jpa/entiy/MemberUseId.java
@@ -7,5 +7,21 @@ import javax.persistence.Id;
 public class MemberUseId {
     @Id
     private Long id;
+    private String name;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- step-3-primarykey-mapping/src/main/java/com/bbubbush/jpa/entiy/MemberUseIdAndGeneratedValue.java
+++ step-3-primarykey-mapping/src/main/java/com/bbubbush/jpa/entiy/MemberUseIdAndGeneratedValue.java
@@ -8,9 +8,27 @@ import javax.persistence.Id;
 @Entity
 public class MemberUseIdAndGeneratedValue {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @GeneratedValue(strategy = GenerationType.AUTO)
 //    @GeneratedValue(strategy = GenerationType.IDENTITY)
 //    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-//    @GeneratedValue(strategy = GenerationType.TABLE)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }

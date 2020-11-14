d2c837b56340d07e79f83c2f682c159976ff0928
bbubbush <bbubbush@gmail.com>
2020-11-14 11:03:02 +0900

    make cascade test code

--- step-12-cascade/src/main/java/com/bbubbush/jpa/entity/Member.java
+++ step-12-cascade/src/main/java/com/bbubbush/jpa/entity/Member.java
@@ -1,11 +1,39 @@
 package com.bbubbush.jpa.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Member {
     @Id @GeneratedValue
     private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name="teamId")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
--- step-12-cascade/src/main/java/com/bbubbush/jpa/entity/Team.java
+++ step-12-cascade/src/main/java/com/bbubbush/jpa/entity/Team.java
@@ -1,11 +1,44 @@
 package com.bbubbush.jpa.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
     @Id @GeneratedValue
-    private Long id;
+    private Long teamId;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team"
+            , cascade = CascadeType.ALL
+//            , orphanRemoval = true
+    )
+    private List<Member> members = new ArrayList<Member>();
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addMember(Member member) {
+        members.add(member);
+        member.setTeam(this);
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
 }
